e0ecac5cfd269d8e55268e8de7d576b0bf61fba0
Tung Beier <44575474+beiertu-mms@users.noreply.github.com>
2023-02-21 17:15:35 +0100

    feat: Add mock-api module to create mappings (#2)

---
+++ mock-api/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.8.10"
+    id("org.jlleitschuh.gradle.ktlint") version "11.2.0"
+
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.http4k:http4k-bom:4.39.0.0"))
+    implementation("org.http4k:http4k-core")
+    implementation("org.http4k:http4k-client-apache")
+
+    implementation("com.github.tomakehurst:wiremock-jre8:2.35.0")
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
+    implementation("ch.qos.logback:logback-classic:1.2.11")
+}
+
+application {
+    mainClass.set("de.beiertu.wiremock.docker.AppKt")
+}
---
+++ mock-api/src/main/kotlin/de/beiertu/wiremock/docker/App.kt
@@ -0,0 +1,53 @@
+package de.beiertu.wiremock.docker
+
+import com.github.tomakehurst.wiremock.client.WireMock.aResponse
+import com.github.tomakehurst.wiremock.client.WireMock.equalTo
+import com.github.tomakehurst.wiremock.client.WireMock.get
+import com.github.tomakehurst.wiremock.client.WireMockBuilder
+import mu.KotlinLogging
+import org.http4k.client.ApacheClient
+import org.http4k.core.Method
+import org.http4k.core.Request
+import java.net.URI
+
+/*
+ * A simple example on how to register requests in WireMock.
+ * See https://wiremock.org/docs/request-matching/ for more information.
+ */
+fun main() {
+    val logger = KotlinLogging.logger {}
+
+    // Wiremock url, which is available after it is started with the provided docker-compose file.
+    // Adjust the scheme and port if needed.
+    val wireMockUri = URI("http://localhost:8080")
+
+    val wireMockClient = WireMockBuilder()
+        .scheme(wireMockUri.scheme)
+        .host(wireMockUri.host)
+        .port(wireMockUri.port)
+        .build()
+
+    // Register a new GET request
+    wireMockClient.register(
+        get("/v2/hello")
+            .withHeader("accept", equalTo("text/plain"))
+            .willReturn(
+                aResponse().withStatus(200).withBody("Hello from v2"),
+            ),
+    )
+
+    // Verify that registered request is working as expected.
+    val httpClient = ApacheClient()
+    val wireMockResponse = httpClient(
+        Request(Method.GET, "$wireMockUri/v2/hello")
+            .header("accept", "text/plain"),
+    )
+
+    logger.info {
+        """
+            |WireMock responds with:
+            |
+            |$wireMockResponse
+        """.trimMargin()
+    }
+}
